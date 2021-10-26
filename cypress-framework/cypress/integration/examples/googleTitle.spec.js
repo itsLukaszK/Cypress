e352a7923917cf5cd94a09f88dc9e351ea6f533b
Lukasz Krawczyk <lukasz.krawczyk@pl.abb.com>
2021-10-26 11:07:48 +0200

    Init

---
+++ cypress-framework/cypress/integration/examples/googleTitle.spec.js
@@ -0,0 +1,7 @@
+
+describe('My First Test', function() {
+    it('Navigates to google.com and verify title', function() {
+        cy.visit('https://www.google.com');
+        cy.title().should('eq', 'Google');
+    })
+})
