f852cc6ff2c768c1235c0231b61a1330982fad9b
Lukasz Krawczyk <lukasz.krawczyk@pl.abb.com>
2021-10-26 11:55:48 +0200

    Cucumber

---
+++ cypress-framework/cypress/integration/common/stepDefinitions/commonSteps.js
@@ -0,0 +1,7 @@
+When('I open Google homepage', () => {
+    cy.visit('https://www.google.com');
+});
+
+Then( 'Title of web page is {string}', (title) => {
+    cy.title().should('include', title);
+});
